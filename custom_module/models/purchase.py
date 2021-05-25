400a636a9e6762395e9ae74738eeac2b042b8538
klausmonga <monga.wakowa@gmail.com>
2021-05-25 14:27:51 +0200

    fisrt

---
+++ custom_module/models/purchase.py
@@ -0,0 +1,16 @@
+from odoo import models, fields, api, _, exceptions
+
+class convention(models.Model):
+    _inherit = ['purchase.requisition']
+
+    def action_purchase_requisition(self):
+        self.ensure_one()
+        purchase_requisition = self.env['purchase.requisition'].search([()])
+        return {
+            "type": "ir.actions.act_window",
+            "res_model": "purchase.requisition",
+            "views": [[False, "kanban"], [False, "list"], [False, "form"], ],
+            "domain": [],
+            "context": {"create": True,},
+            "name": _("purchase_requisition"),
+        }
