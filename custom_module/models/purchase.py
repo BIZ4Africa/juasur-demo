1f187810a7ac4ec5f0313335845b7c6271d34f02
klausmonga <monga.wakowa@gmail.com>
2021-05-25 15:16:57 +0200

    wip

--- custom_module/models/purchase.py
+++ custom_module/models/purchase.py
@@ -5,7 +5,11 @@ class purchase_requisition(models.Model):
 
     purchase_requisition_count = fields.Integer(string='purchase requisition', compute='_purchase_requisition_count', store=False)
     # purchase_requisition_ids = fields.One2many('purchase.requisition', 'lead_id', string='purchase requisition IDs', copy=False)
-
+    purchase_requisition_ids = fields.One2many(
+        string="purchase requisitions",
+        comodel_name="purchase.requisition",
+        inverse_name="purchase_requisition_id",
+    )
     def _purchase_requisition_count(self):
         for lead in self:
             lead.purchase_requisition_count = self.env['purchase.requisition'].search_count([])
